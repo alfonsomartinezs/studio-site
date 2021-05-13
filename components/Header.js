a0a51cb535d815ec3fe2eeec53d7d41fc5ab8062
Alfonso <61892420+ShrikeFound@users.noreply.github.com>
2021-05-12 23:12:28 -0700

    added header component, mobile menu

--- components/Header.js
+++ components/Header.js
@@ -1,20 +1,26 @@
 import { useState } from "react";
-
+import Link from 'next/link'
 const Header = () => {
   const [show,setShow] = useState(false)
   const toggleMenu = () => {
     setShow(!show)
   }
+
+  const closeMenu = () => {
+    setShow(false)
+  }
+
+
   console.log(show)
   return (
     <header className="container">
-      <h3>LOGO</h3>
-      <span onClick={toggleMenu}>Menu</span>
+      <span onClick={ closeMenu}><Link href="/" >LOGO</Link></span>
+      <span onClick={toggleMenu}>{ show ? "Close": "Menu"}</span>
       <nav className={ show ? "show": ""}>
         <ul className="nav-list">
-          <li className="nav-list-item">Portfolio</li>
-          <li className="nav-list-item">About Us</li>
-          <li className="nav-list-item">Contact</li>
+          <li className="nav-list-item" onClick={closeMenu}><Link href="/portfolio">Portfolio</Link></li>
+          <li className="nav-list-item" onClick={closeMenu}><Link href="/about">About Us</Link></li>
+          <li className="nav-list-item" onClick={closeMenu}><Link href="/contact">Contact</Link></li>
         </ul>
       </nav>
     </header>
