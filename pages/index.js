1956e88c8f4ee97a6a1b72896a97c1890d06ba16
Alfonso <61892420+ShrikeFound@users.noreply.github.com>
2021-05-16 08:22:22 -0700

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,20 @@
 import Head from 'next/head'
 import Image from 'next/image'
-import Footer from '../components/footer'
-import Header from '../components/header'
+import Footer from '../components/Footer'
+import Header from '../components/Header'
 import styles from '../styles/Home.module.css'
 
 export default function Home() {
   return (
-      <h1>Home</h1>
+    <section>
+      <h1>Project Paramour</h1>
+      <p>
+        Project made for an art museum near Southwest London. Project Paramour is a statement of bold, modern architecture.
+      </p>
+      <a className="button">
+         <span>See our Portfolio</span>
+           <img src="/assets/icon-arrow.svg"/>     
+      </a>
+      </section>
   )
 }
